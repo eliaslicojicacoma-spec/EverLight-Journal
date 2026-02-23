17d98db158a83a0bebf51951cf3269ceeb75a1b2
eliaslicojicacoma-spec <eliaslicojicacoma@gmail.com>
2026-02-23 22:40:04 +0100

    Add monetization configuration file

---
+++ src/config/monetizationConfig.ts
@@ -0,0 +1,5 @@
+export const monetizationConfig = {
+  pixKey: "elias-licoji-cacoma-273@jim.com",
+  paypalEmail: "eliaslicojicacoma@gmail.com"
+  // AdSense IDs entram aqui depois que o Google aprovar (ex: ca-pub-XXXXXXXX)
+};
