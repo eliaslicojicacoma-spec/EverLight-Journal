652488be998ac0cad473418e256daea428d51d20
eliaslicojicacoma-spec <eliaslicojicacoma@gmail.com>
2026-02-28 09:15:05 +0100

    Create socialConfig.ts

---
+++ src/config/socialConfig.ts
@@ -0,0 +1,7 @@
+export const socialConfig = {
+  facebook: "https://www.facebook.com/share/1L9YyUSD2x/",
+  instagram: "https://www.instagram.com/eliascacoma?igsh=bmM3MHg3NXp2Z3h3",
+  youtube: "https://youtube.com/@eliaslicojicacoma?si=WX-GbCP0z9kW2vV4",
+  // Se tiveres LinkedIn depois, colocas aqui:
+  linkedin: "",
+};
