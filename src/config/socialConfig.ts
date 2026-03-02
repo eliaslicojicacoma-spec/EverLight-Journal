71eb3ce2d06d43ce2cd967e73e2fb029f7ee7687
eliaslicojicacoma-spec <eliaslicojicacoma@gmail.com>
2026-03-02 01:17:40 +0100

    Update socialConfig.ts

--- src/config/socialConfig.ts
+++ src/config/socialConfig.ts
@@ -1,7 +1,5 @@
 export const socialConfig = {
-  facebook: "https://www.facebook.com/share/1L9YyUSD2x/",
+  facebook: "https://www.facebook.com/share/1DeT9sjHyp/",
   instagram: "https://www.instagram.com/eliascacoma?igsh=bmM3MHg3NXp2Z3h3",
-  youtube: "https://youtube.com/@eliaslicojicacoma?si=WX-GbCP0z9kW2vV4",
-  // Se tiveres LinkedIn depois, colocas aqui:
-  linkedin: "",
+  youtube: "https://youtube.com/@eliaslicojicacoma?si=VXF4ITBz_J8diK1c",
 };
