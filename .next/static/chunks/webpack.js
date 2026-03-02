b4e30c8c36ea46a19afedc235849514cfc5dc06a
eliaslicojicacoma-spec <eliaslicojicacoma@gmail.com>
2026-03-02 19:45:29 +0000

    chore: scaffold v2 folder structure

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "43e0603698061b30"; }
+/******/ 		__webpack_require__.h = function() { return "8662522c9a48c684"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
