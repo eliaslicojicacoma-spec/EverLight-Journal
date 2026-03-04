9799f25f466ad8a19e00157a4b32846ac5b90b81
eliaslicojicacoma-spec <eliaslicojicacoma@gmail.com>
2026-03-04 01:14:36 +0100

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,1 +1,34 @@
+import { MetadataRoute } from "next";
+import { siteConfig } from "@/config/siteConfig";
+import { getBlogArticles } from "@/content/blog/articles";
 
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = siteConfig.url;
+
+  const articles = getBlogArticles();
+
+  const blogUrls = articles.map((article) => ({
+    url: `${baseUrl}/pt/blog/${article.slug}`,
+    lastModified: new Date(article.date),
+  }));
+
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/pt/blog`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/pt/blog/categories`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/pt/blog/tags`,
+      lastModified: new Date(),
+    },
+    ...blogUrls,
+  ];
+}
