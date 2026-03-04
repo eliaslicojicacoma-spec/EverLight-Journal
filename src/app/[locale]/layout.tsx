e9587aa6868ade7e3499757cc8c30d9d0e9bb17d
eliaslicojicacoma-spec <eliaslicojicacoma@gmail.com>
2026-03-04 18:52:03 +0100

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,17 +1,5 @@
-import "@/styles/globals.css";
-import type { Metadata } from "next";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
-import { siteConfig } from "@/config/siteConfig";
-
-export const metadata: Metadata = {
-  metadataBase: new URL(siteConfig.url),
-  title: {
-    default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-};
 
 export default function LocaleLayout({
   children,
@@ -21,12 +9,10 @@ export default function LocaleLayout({
   params: { locale: string };
 }) {
   return (
-    <html lang={params.locale} suppressHydrationWarning>
-      <body className="min-h-dvh bg-zinc-950 text-zinc-100 antialiased">
-        <Header />
-        <main className="py-10">{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <>
+      <Header />
+      <main className="py-10">{children}</main>
+      <Footer />
+    </>
   );
 }
